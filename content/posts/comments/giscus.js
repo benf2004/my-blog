df86c2a24bcf40efa7558334539b8ecf8f972573
Ben <ben@phooey.foo>
2023-07-01 17:17:06 -0600

    vault backup: 2023-07-01 17:17:06

--- content/posts/comments/giscus.js
+++ content/posts/comments/giscus.js
@@ -1,21 +1,21 @@
+import * as React from "react"
 import Giscus from '@giscus/react';
 
 export default function Comments() {
     return (
         <Giscus
             id="comments"
-            repo="giscus/giscus-component"
-            repoId="MDEwOlJlcG9zaXRvcnkzOTEzMTMwMjA="
+            repo="benf2004/my-blog"
+            repoId="R_kgDOJx_n5Q"
             category="Announcements"
-            categoryId="DIC_kwDOF1L2fM4B-hVS"
-            mapping="specific"
+            categoryId="DIC_kwDOJx_n5c4CXmy-"
+            mapping="pathname"
             term="Welcome to @giscus/react component!"
             reactionsEnabled="1"
             emitMetadata="0"
             inputPosition="top"
             theme="light"
             lang="en"
-            loading="lazy"
         />
     );
 }
