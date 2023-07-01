bbda64721ac55f4e02984f3d8ff6d59f97c3d189
Ben <ben@phooey.foo>
2023-07-01 16:08:17 -0600

    configuration

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -50,9 +50,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        name: `Ben's Blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `A blog by Ben Finch about entrepreneurship, tech, Mormonism, and my life.`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
