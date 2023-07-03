6c8c657dba19dc248622391e375f599f552e2ac4
Ben <ben@phooey.foo>
2023-07-02 18:00:37 -0600

    comments component

---
+++ content/comments/Comments.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+
+const Comments = () => {
+    useEffect(() => {
+        function changeGiscusTheme() {
+            const theme = localStorage.getItem('theme-ui-color-mode');
+
+            function sendMessage(message) {
+                const iframe = document.querySelector('iframe.giscus-frame');
+                if (!iframe) return;
+                iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
+            }
+
+            sendMessage({
+                setConfig: {
+                    theme: theme,
+                },
+            });
+        }
+
+        changeGiscusTheme();
+
+        document.querySelector('button').addEventListener('click', changeGiscusTheme);
+
+        return () => {
+            document.querySelector('button').removeEventListener('click', changeGiscusTheme);
+        };
+    }, []);
+
+    return (
+        <Giscus
+            id="comments"
+            repo="benf2004/my-blog"
+            repoId="R_kgDOJx_n5Q"
+            category="Announcements"
+            categoryId="DIC_kwDOJx_n5c4CXmy-"
+            mapping="pathname"
+            term="Welcome to @giscus/react component!"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme="preferred_color_scheme"
+            lang="en"
+        />
+    );
+};
+
+export default Comments;
